3dac9e2c35af42a88ed297fbefc41c1ad9628c5c
mkhizes <lubanzi01trading@gmail.com>
2024-02-29 19:43:19 +0200

    corrected variables

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -16,13 +16,13 @@ class Program
     
         
         Console.Write("What is your name?  ");
-        string first_name = Console.ReadLine();
+        string firstName = Console.ReadLine();
         
 
         Console.Write("What is your last name?  ");
-        string last_name = Console.ReadLine();
+        string lastName = Console.ReadLine();
         
 
-        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}. ");
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}. ");
     }
 }
