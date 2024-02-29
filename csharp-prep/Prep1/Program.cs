e6a70509d2348ceb37c0bcb1390a523f0498492a
mkhizes <lubanzi01trading@gmail.com>
2024-02-29 20:08:26 +0200

    made a few changes in prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,16 +4,7 @@ class Program
 {
     static void Main(string[] args)
     { //Ask for their name
-        Console.Write("What is your name?");
-        string firstname = "Scott";
-        Console.WriteLine(firstname);
-
-        Console.Write("What is your last name?");
-        string lastname = "Burton";
-        Console.WriteLine(lastname);
-
-        Console.WriteLine($"Your name is Burton,{firstname} {lastname} ");
-    
+                 
         
         Console.Write("What is your name?  ");
         string firstName = Console.ReadLine();
