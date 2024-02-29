c60c6bac710133c524779e156666ea852e088d4d
mkhizes <lubanzi01trading@gmail.com>
2024-02-29 16:38:23 +0200

    Corrected mistakes

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("What is your name?");
+        Console.Write("What is your name?");
+        string firstname = "Scott";
+        Console.Write(firstname);
+
+        Console.Write("What is your last name?");
+        string lastname = "Burton";
+        Console.Write(lastname);
+
+        Console.WriteLine($"Your name is Burton,{firstname} {lastname} ");
+    
+    
     }
 }
