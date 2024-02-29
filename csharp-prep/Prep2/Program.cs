a8e71f5ede9c2f8a6bd533a70ba9b047694275d3
mkhizes <lubanzi01trading@gmail.com>
2024-02-29 21:10:26 +0200

    Added code in prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter Percentage you obtained? ");
+        string UserValue= Console.ReadLine();
+        int x = int.Parse(UserValue);
+
+        if (x>=90)
+        {
+            Console.Write(" Congratulation you got A");
+        }
+        else if (x>=80)
+        {
+            Console.Write("Congratulation you got B");
+        }
+        else if(x>=70)
+        {
+            Console.Write("Congratulation you got C");
+        }
+        else if(x<70)
+        {
+            Console.Write("Sorry, put more effort next time ");
+        }
     }
 }
