508ee3e30c1d6544e696a52c673c75dd231c5a83
mkhizes <lubanzi01trading@gmail.com>
2024-02-29 17:52:27 +0200

    added a comment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -3,7 +3,7 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
+    { //Ask for their name
         Console.Write("What is your name?");
         string firstname = "Scott";
         Console.WriteLine(firstname);
@@ -15,14 +15,14 @@ class Program
         Console.WriteLine($"Your name is Burton,{firstname} {lastname} ");
     
         
-        Console.Write("What is your name?");
+        Console.Write("What is your name?  ");
         string first_name = Console.ReadLine();
         
 
-        Console.Write("What is your last name?");
+        Console.Write("What is your last name?  ");
         string last_name = Console.ReadLine();
         
 
-        Console.WriteLine($"Your name is {last_name},{first_name} {last_name}. ");
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}. ");
     }
 }
