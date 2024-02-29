922a0eb497c526e22e86313e5c13bec7d72680d5
mkhizes <lubanzi01trading@gmail.com>
2024-02-29 16:11:52 +0200

    added a question for user

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,13 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-    
-        Console.WriteLine("What is your first name?");
-        string firstname=Console.Readline(firstname);
-
-        Console.WriteLine(" What is your last name?");
-        string lastname=Console.Readline();
-
-        Console.WriteLine($"Your name is {lastname},{firstname} + {lastname}");
+        Console.WriteLine("What is your name?");
     }
 }
