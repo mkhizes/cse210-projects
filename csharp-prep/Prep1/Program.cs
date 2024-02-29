b13abb1d2fb7669160d9095990a74cb0d328f4c0
mkhizes <lubanzi01trading@gmail.com>
2024-02-29 17:36:03 +0200

    coded accordingly twice

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,24 +5,24 @@ class Program
     static void Main(string[] args)
     {
         Console.Write("What is your name?");
-        string firstname = Console.Read();
+        string firstname = "Scott";
         Console.WriteLine(firstname);
 
         Console.Write("What is your last name?");
-        string lastname = Console.Read();
+        string lastname = "Burton";
         Console.WriteLine(lastname);
 
-        Console.WriteLine($"Your name is {lastname},{firstname} {lastname} ");
+        Console.WriteLine($"Your name is Burton,{firstname} {lastname} ");
     
         
         Console.Write("What is your name?");
         string first_name = Console.ReadLine();
-        Console.WriteLine(first_name);
+        
 
         Console.Write("What is your last name?");
         string last_name = Console.ReadLine();
-        Console.WriteLine(last_name);
+        
 
-        Console.WriteLine($"Your name is {last_name},{first_name} {last_name} ");
+        Console.WriteLine($"Your name is {last_name},{first_name} {last_name}. ");
     }
 }
