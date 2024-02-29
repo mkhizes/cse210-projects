f7c8beae3df42b41505b5fb460d6696fd89f6944
mkhizes <lubanzi01trading@gmail.com>
2024-02-29 15:46:27 +0200

    edited and added code

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,11 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+    
         Console.WriteLine("What is your first name?");
-        string firstname=Console.Readline();
+        string firstname=Console.Readline(firstname);
+
         Console.WriteLine(" What is your last name?");
-        string lastname=Console.Readline()
-        Console.WriteLine($"Your name is {lastname},{firstname} + {lastname}")
+        string lastname=Console.Readline();
+
+        Console.WriteLine($"Your name is {lastname},{firstname} + {lastname}");
     }
 }
