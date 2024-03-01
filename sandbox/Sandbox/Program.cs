61746de970a0f0c3e7df6b5e137cdd04e12c20ba
mkhizes <lubanzi01trading@gmail.com>
2024-03-01 11:41:35 +0200

    Undone a code to fix errors

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,9 +5,6 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World!");
-        for (int i=2; i<=20; i=i+2)
-        {
-            Console.WriteLine(i);
-        }
+        
     }
 }
