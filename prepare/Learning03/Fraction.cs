da19ae96f3c4309de230ec49a3862672cc4a83f7
mkhizes <lubanzi01trading@gmail.com>
2024-03-12 02:04:57 +0200

    added some changes in a file

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,34 @@
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+    public Fraction()
+    {
+         _top=1;
+        _bottom=1;
+    }
+    public Fraction(int wholeNumber)
+    {
+        _top=wholeNumber;
+        _bottom=1;
+    }
+    public Fraction(int top, int bottom)
+    {
+        _top=top;
+        _bottom=bottom;
+    }
+    public string GetFractionstring()
+    {
+        string text = $"{_top}/{_bottom}";
+        return text;
+    }
+    
+    public double GetDecimalValue()
+    {
+        // Notice that this is not stored as a member variable.
+        // Is will be recomputed each time this is called.
+        return (double)_top / (double)_bottom;
+    }
+    
+
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,5 +5,26 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+        Fraction f1=new Fraction();
+        Console.WriteLine(f1.GetFractionstring());
+        Console.WriteLine(f1.GetFractionstring());
+
+        Fraction f2=new Fraction(5);
+        Console.WriteLine(f2.GetFractionstring());
+        Console.WriteLine(f2.GetDecimalValue());
+
+        Fraction f3=new Fraction(3,4);
+        Console.WriteLine(f3.GetFractionstring());
+        Console.WriteLine(f3.GetDecimalValue());
+
+        Fraction f4=new Fraction(1,3);
+        Console.WriteLine(f4.GetFractionstring());
+        Console.WriteLine(f4.GetDecimalValue());
+
+        Fraction f5=new Fraction(2,5);
+        Console.WriteLine(f5.GetFractionstring());
+        Console.WriteLine(f5.GetDecimalValue());
     }
+    
+    
 }
