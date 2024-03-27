7a3cd1c680d7c87f377cb1eb011033edd58f3133
mkhizes <lubanzi01trading@gmail.com>
2024-03-27 11:07:07 +0200

    completed the exercise and made few changes

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Red Square", 6);
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle("Yellow Rectangle", 8, 10);
+        shapes.Add(s2);
+
+        Circle s3 = new Circle("Green Circle", 12);
+        shapes.Add(s3);
+
+        foreach (Shape s in shapes)
+        {
+            
+            string color = s.GetColor();
+
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
